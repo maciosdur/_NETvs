f82ecd5adc3a3f3869170c66e76b3f50378fb88b
maciosdur <maciej.durkalec@gmail.com>
2025-12-14 15:08:44 +0100

    l8

--- lab8/lab8/Controllers/GameController.cs
+++ lab8/lab8/Controllers/GameController.cs
@@ -5,14 +5,12 @@ namespace lab8.Controllers
 {
     public class GameController : Controller
     {
-        // --- ZMIENNE STATYCZNE PRZECHOWUJĄCE STAN GRY ---
-        private static int RangeN = 100; // Domyślny zakres, jeśli Set nie zostanie wywołane
+    
+        private static int RangeN = 100;
         private static int RandValue;
         private static int AttemptsCount = 0;
         private static readonly Random Rnd = new Random();
 
-        // Akcja wywoływana przez /Set,N
-        // Używamy atrybutu [Route] dla specyficznych wzorców
         [Route("Set,{n}")]
         public IActionResult Set(int n)
         {
@@ -24,29 +22,25 @@ namespace lab8.Controllers
             }
 
             RangeN = n;
-            AttemptsCount = 0; // Resetujemy próby
-            // Losowanie nowej liczby od razu po ustawieniu zakresu jest dobrą praktyką
+            AttemptsCount = 0;
             RandValue = Rnd.Next(0, RangeN);
 
-            ViewBag.Message = $"Ustawiono nowy zakres losowania $n={RangeN}$. Wylosowano nową liczbę (0 do $n-1$).";
+            ViewBag.Message = $"Ustawiono nowy zakres losowania n={RangeN}. Wylosowano nową liczbę (0 do n-1).";
             ViewBag.CssClass = "set-state";
             return View("GameStatus");
         }
 
-        // Akcja wywoływana przez /Draw
         [Route("Draw")]
         public IActionResult Draw()
         {
-            // Losowanie nowej liczby z aktualnie ustawionego zakresu (RangeN)
             RandValue = Rnd.Next(0, RangeN);
-            AttemptsCount = 0; // Zerowanie licznika prób
+            AttemptsCount = 0;
 
             ViewBag.Message = $"Wylosowano nową liczbę w zakresie od 0 do {RangeN - 1}. Rozpocznij zgadywanie!";
             ViewBag.CssClass = "draw-state";
             return View("GameStatus");
         }
 
-        // Akcja wywoływana przez /Guess,X
         [Route("Guess,{guess}")]
         public IActionResult Guess(int guess)
         {
@@ -59,18 +53,15 @@ namespace lab8.Controllers
             {
                 ViewBag.Message = $"Brawo! Zgadłeś liczbę {RandValue} w {AttemptsCount} próbach.";
                 ViewBag.CssClass = "win-state";
-                // Można tu wylosować nową liczbę, aby przygotować następną grę
-                // RandValue = Rnd.Next(0, RangeN);
-                // AttemptsCount = 0;
             }
             else if (guess < RandValue)
             {
-                ViewBag.Message = $"Za mała liczba. Próbuj dalej!";
+                ViewBag.Message = $"{guess} - Za mała liczba. Próbuj dalej!";
                 ViewBag.CssClass = "low-state";
             }
-            else // guess > RandValue
+            else 
             {
-                ViewBag.Message = $"Za duża liczba. Próbuj dalej!";
+                ViewBag.Message = $"{guess} - Za duża liczba. Próbuj dalej!";
                 ViewBag.CssClass = "high-state";
             }
 
--- lab8/lab8/Controllers/ToolController.cs
+++ lab8/lab8/Controllers/ToolController.cs
@@ -1,62 +1,168 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using System;
+
+//namespace lab8.Controllers
+//{
+//    public class ToolController : Controller
+//    {
+//        [Route("Tool/Solve/{a}/{b}/{c}")]
+//        public IActionResult Solve(double a, double b, double c)
+//        {
+//            string message;
+//            string cssClass;
+
+//            if (a == 0 && b == 0 && c == 0)
+//            {
+//                message = "Równanie tożsamościowe – nieskończenie wiele rozwiązań.";
+//                cssClass = "infinite";
+//            }
+//            else if (a == 0 && b == 0)
+//            {
+//                message = "Brak rozwiązań.";
+//                cssClass = "none";
+//            }
+//            else if (a == 0)
+//            {
+//                // równanie liniowe
+//                double x = -c / b;
+//                message = $"Równanie liniowe. Rozwiązanie: x = {x}";
+//                cssClass = "one";
+//            }
+//            else
+//            {
+//                // równanie kwadratowe
+//                double delta = b * b - 4 * a * c;
+
+//                if (delta > 0)
+//                {
+//                    double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+//                    double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+//                    message = $"Dwa rozwiązania: x₁ = {x1}, x₂ = {x2}";
+//                    cssClass = "two";
+//                }
+//                else if (delta == 0)
+//                {
+//                    double x = -b / (2 * a);
+//                    message = $"Jedno rozwiązanie: x = {x}";
+//                    cssClass = "one";
+//                }
+//                else
+//                {
+//                    message = "Brak rozwiązań rzeczywistych (Δ < 0).";
+//                    cssClass = "none";
+//                }
+//            }
+
+//            ViewBag.Message = message;
+//            ViewBag.CssClass = cssClass;
+
+//            return View();
+//        }
+//    }
+//}
+
+
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic; 
 
 namespace lab8.Controllers
 {
+    public class SolutionResult
+    {
+        public int Count { get; set; } // Liczba rozwiązań (0, 1, 2, lub -1 dla tożsamościowego)
+        public List<double> Solutions { get; set; } = new List<double>();
+    }
+
     public class ToolController : Controller
     {
         [Route("Tool/Solve/{a}/{b}/{c}")]
         public IActionResult Solve(double a, double b, double c)
         {
-            string message;
-            string cssClass;
+            SolutionResult result = SolveQuadraticEquation(a, b, c);
+
+            SetViewData(a, b, c, result);
+
+            return View();
+        }
+
+        // ---------------------------------
+        private SolutionResult SolveQuadraticEquation(double a, double b, double c)
+        {
+            var result = new SolutionResult();
 
             if (a == 0 && b == 0 && c == 0)
             {
-                message = "Równanie tożsamościowe – nieskończenie wiele rozwiązań.";
-                cssClass = "infinite";
+                result.Count = -1;
             }
             else if (a == 0 && b == 0)
             {
-                message = "Brak rozwiązań.";
-                cssClass = "none";
+                result.Count = 0;
             }
             else if (a == 0)
             {
-                // równanie liniowe
-                double x = -c / b;
-                message = $"Równanie liniowe. Rozwiązanie: x = {x}";
-                cssClass = "one";
+                result.Solutions.Add(-c / b);
+                result.Count = 1;
             }
             else
             {
-                // równanie kwadratowe
                 double delta = b * b - 4 * a * c;
 
                 if (delta > 0)
                 {
-                    double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
-                    double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
-                    message = $"Dwa rozwiązania: x₁ = {x1}, x₂ = {x2}";
-                    cssClass = "two";
+                    result.Solutions.Add((-b + Math.Sqrt(delta)) / (2 * a));
+                    result.Solutions.Add((-b - Math.Sqrt(delta)) / (2 * a));
+                    result.Count = 2;
                 }
                 else if (delta == 0)
                 {
-                    double x = -b / (2 * a);
-                    message = $"Jedno rozwiązanie: x = {x}";
-                    cssClass = "one";
+                    result.Solutions.Add(-b / (2 * a));
+                    result.Count = 1;
                 }
                 else
                 {
-                    message = "Brak rozwiązań rzeczywistych (Δ < 0).";
-                    cssClass = "none";
+                    result.Count = 0;
                 }
+            }
+
+            return result;
+        }
+
+        // ------------------------
+        private void SetViewData(double a, double b, double c, SolutionResult result)
+        {
+            string message;
+            string cssClass;
+
+            string equation = $"{a}x² + {b}x + {c} = 0";
+
+            if (result.Count == -1)
+            {
+                message = $"Równanie tożsamościowe: {equation} – nieskończenie wiele rozwiązań.";
+                cssClass = "infinite";
+            }
+            else if (result.Count == 2)
+            {
+                double x1 = result.Solutions[0];
+                double x2 = result.Solutions[1];
+                message = $"Dwa rozwiązania dla {equation}: x₁ = {x1}, x₂ = {x2}";
+                cssClass = "two";
+            }
+            else if (result.Count == 1)
+            {
+                double x = result.Solutions[0];
+                string type = (a == 0) ? "liniowe" : "kwadratowe";
+                message = $"Jedno rozwiązanie ({type}) dla {equation}: x = {x}";
+                cssClass = "one";
+            }
+            else
+            {
+                message = $"Brak rozwiązań rzeczywistych dla {equation}.";
+                cssClass = "none";
             }
 
             ViewBag.Message = message;
             ViewBag.CssClass = cssClass;
-
-            return View();
         }
     }
 }
